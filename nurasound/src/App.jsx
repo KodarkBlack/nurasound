be71a40fa73352b3f1c6d139c5a609cb55c23595
Olufisayo Israel Tomide <115098511+KodarkBlack@users.noreply.github.com>
2023-06-29 09:28:26 +0100

    fix

---
+++ nurasound/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ nurasound/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between ml-[5%]'>
+      <ul className="text-startup-grey/40 text-[17px] flex items-center justify-center gap-8 ">
+        <li className="text-3xl">Nura</li>
+        <li>Product</li>
+        <li>Subscription</li>
+        <li>Why Nura</li>
+        <li>Support</li>
+      </ul>
+      <h1 className='mr-[5%]'>Cart (0)</h1>
+    </div>
+  )
+}
+
+export default Navbar
